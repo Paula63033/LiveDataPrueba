86b329b58bd502407a4c04c28949a2dc344d682c
didacflorensa <didacflorensacazorla@gmail.com>
2022-06-02 09:54:43 +0200

    Comments Didac

--- app/src/main/java/com/example/livedataprueba/activity/NameActivity.java
+++ app/src/main/java/com/example/livedataprueba/activity/NameActivity.java
@@ -6,12 +6,14 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.example.livedataprueba.R;
 import com.example.livedataprueba.databinding.ContentMainBinding;
 
 import com.example.livedataprueba.viewmodel.NameViewModel;
 
 public class NameActivity extends AppCompatActivity {
-    private NameViewModel model;
+    private NameViewModel viewmodel;
     private ContentMainBinding binding;
 
     @Override
@@ -20,8 +22,15 @@ public class NameActivity extends AppCompatActivity {
 
         binding = ContentMainBinding.inflate(getLayoutInflater());
 
+        // Set the Content of the xml to the view
+        //TODO: @Didac. Necessites tenir el fitxer xml (la vista on hi ha botons, etc...) referenciat. Següent linea
+        setContentView(binding.getRoot());
+
         //Get the ViewModel
-        model = new ViewModelProvider(this).get(NameViewModel.class);
+        viewmodel = new ViewModelProvider(this).get(NameViewModel.class);
+
+        //TODO @Didac. En aquesta funció li passem al binding el viewmodel que treballarà aquests mutables i li indiquem
+        setup();
 
 
         /*
@@ -38,6 +47,12 @@ public class NameActivity extends AppCompatActivity {
         model.getCurrentName().observe(this, nameObserver);
 
          */
+    }
+
+    //TODO @Didac. En aquesta funció li passem al binding el viewmodel que treballarà aquests mutables i li indiquem
+    private void setup(){
+        binding.setViewModel(viewmodel);
+        binding.setLifecycleOwner(this);
     }
 
 }
